b52afe1abbf0510314ceb9d9a4e4cdf6fe84d515
Joonhee Jeong <jeonggoo75@gmail.com>
2023-06-22 14:52:11 +0900

    #implement #8 - SingletonService class
    SingletonService를 만들어서 싱글톤이 적용되었는지 테스트합니다.

--- src/test/java/singleton/SingletonTest.java
+++ src/test/java/singleton/SingletonTest.java
@@ -19,7 +19,19 @@ public class SingletonTest {
 
         System.out.println("memberService1 : " + memberService1);
         System.out.println("memberService2 : " + memberService2);
-        
+
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonObject() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 : " + singletonService1);
+        System.out.println("singletonService2 : " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
