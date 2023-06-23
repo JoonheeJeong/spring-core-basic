469e5b77f600a439279fd3bbc1bd9cfc604e2a55
Joonhee Jeong <jeonggoo75@gmail.com>
2023-06-23 16:09:06 +0900

    test #10 - duplicate beans and conflict
    - 자동 빈 vs 자동 빈은 예외 발생
    - 수동 빈 vs 자동 빈은 수동 빈이 오버라이딩함
    - 자동 주입 후보가 여럿일 경우의 문제 해결

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -2,8 +2,11 @@ package hello.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+//@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = AppConfig.class))
 public class CoreApplication {
 
 	public static void main(String[] args) {
