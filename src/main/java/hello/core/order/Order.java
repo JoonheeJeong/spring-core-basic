15d8bd35a6d4f7e592f5da869a0dba49224af16f
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-03 21:50:07 +0900

    use #12 - lombok
    롬복 라이브러리를 사용하여 반복 작성되는 코드를 줄입니다.

--- src/main/java/hello/core/order/Order.java
+++ src/main/java/hello/core/order/Order.java
@@ -1,5 +1,14 @@
 package hello.core.order;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Order {
 
     private Long memberId;
@@ -7,56 +16,7 @@ public class Order {
     private int itemPrice;
     private int discountAmount;
 
-    public Order(Long memberId, String itemName, int itemPrice, int discountAmount) {
-        this.memberId = memberId;
-        this.itemName = itemName;
-        this.itemPrice = itemPrice;
-        this.discountAmount = discountAmount;
-    }
-
     public int calculatePrice() {
         return itemPrice - discountAmount;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-
-    public int getItemPrice() {
-        return itemPrice;
-    }
-
-    public void setItemPrice(int itemPrice) {
-        this.itemPrice = itemPrice;
-    }
-
-    public int getDiscountAmount() {
-        return discountAmount;
-    }
-
-    public void setDiscountAmount(int discountAmount) {
-        this.discountAmount = discountAmount;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "memberId=" + memberId +
-                ", itemName='" + itemName + '\'' +
-                ", itemPrice=" + itemPrice +
-                ", discountAmount=" + discountAmount +
-                '}';
     }
 }
