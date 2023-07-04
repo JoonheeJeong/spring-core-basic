9809ae45834c8a7f0461eddf302d2aae5e65a75b
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-04 16:40:06 +0900

    modify #14 - to apply interface callback
    스프링 전용 인터페이스를 구현하여, 
    1. 속성 초기화 후
    2. 객체 소멸 전
    의 콜백 함수를 등록합니다.

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("constructor, url: " + url);
-        connect();
-        call("초기화 연결");
     }
 
     public void setUrl(String url) {
@@ -24,5 +25,18 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("disconnect: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
