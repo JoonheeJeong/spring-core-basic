f05d4bd9490829b326478e37486d4e17700e6d06
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-05 15:07:08 +0900

    fix #16 - ScopeNotActiveException by using
    Provider를 사용하여 예외를 해결합니다.

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -5,18 +5,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Provider;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 public class LogDemoController {
 
+    private final Provider<MyLogger> myLoggerProvider;
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
 
     @GetMapping("log-demo")
-    public String logDemo(HttpServletRequest req) {
+    public String logDemo(HttpServletRequest req) throws InterruptedException {
+        MyLogger myLogger = myLoggerProvider.get();
         myLogger.setRequestURL(req.getRequestURL().toString());
+
+        Thread.sleep(1000);
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
