911a8cd12c90854bcbeb84f72867aacc4b2bebce
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-05 14:20:39 +0900

    test, modify #16 - to use JSR-330 Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -84,11 +85,13 @@ public class SingletonWithPrototypeTest {
     @RequiredArgsConstructor
     private static class ClientBeanWithProvider {
 
-        private final ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;
+//        private final ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;
+        private final Provider<PrototypeBean> prototypeBeanProvider;
 
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
+//            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
